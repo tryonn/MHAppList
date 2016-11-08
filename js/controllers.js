6b52074acff27a6124ce925ffb77a651f2d15061
Tiago Vale <tiago_oliveira914@hotmail.com>
2016-11-08 14:46:11 -0200

    Reajustado os filtros por nome e o nome real.

--- js/controllers.js
+++ js/controllers.js
@@ -3,6 +3,36 @@ var characterControllers = angular.module('characterControllers', []);
 characterControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
     $http.get('js/characters.json').success(function(data) {
         $scope.characters = data;
-        
+        $scope.filterSelection = 'name';
+
+
+
     });
+    $scope.toggleSelection = function() {
+        if ($scope.filterSelection === 'name') {
+            $scope.filterSelection = 'name_real';
+        } else {
+            $scope.filterSelection = 'name';
+        }
+
+
+    }
+
+    $scope.queryFilter = function(item) {
+        if ($scope.queryText == undefined) {
+            return true;
+        } else {
+
+            if ($scope.filterSelection === 'name' && item.name.toLowerCase().indexOf($scope.queryText.toLowerCase()) != -1) {
+                return true;
+            }
+            if ($scope.filterSelection === 'name_real' && item.name_real.toLowerCase().indexOf($scope.queryText.toLowerCase()) != -1) {
+                return true;
+            }
+
+        }
+        return false;
+    }
+
+
 }]);
