1558f735f48979305330b37be91cd95cc81def20
Tiago Vale <tiago_oliveira914@hotmail.com>
2016-11-18 17:29:02 -0200

    Implementado a passagem de parâmetros para o modal de exclusão.

--- js/controllers.js
+++ js/controllers.js
@@ -1,21 +1,25 @@
 var characterControllers = angular.module('characterControllers', ['angularModalService', 'ngAnimate']);
 
-characterControllers.controller('CustomController', ['$scope', 'close', function($scope, close) {
+characterControllers.controller('CustomController', ['$scope', 'close', 'item', function($scope, close, item) {
 
-console.log('CustomController');
- $scope.close = function(result) {
-      close(result, 500); // close, but give 500ms for bootstrap to animate
-  };
+    $scope.item = item;
+
+    $scope.close = function(result) {
+        close(result, 500); // close, but give 500ms for bootstrap to animate
+    };
 
 
 }]);
 
 
-characterControllers.controller('ListController', ['$scope','ModalService', '$http', function($scope,ModalService, $http) {
+characterControllers.controller('ListController', ['$scope', 'ModalService', '$http', function($scope, ModalService, $http) {
     $http.get('js/characters.json').success(function(data) {
 
         $scope.characters = data;
-        $scope.filterSelection = {text : 'Nome', value: 'name'};
+        $scope.filterSelection = {
+            text: 'Nome',
+            value: 'name'
+        };
         $scope.order;
         $scope.msgDelete;
 
@@ -24,16 +28,25 @@ characterControllers.controller('ListController', ['$scope','ModalService', '$ht
     $scope.toggleSelection = function() {
         if ($scope.filterSelection.value === 'name') {
 
-            $scope.filterSelection = {text:'Nome Real', value:'real_name'};
+            $scope.filterSelection = {
+                text: 'Nome Real',
+                value: 'real_name'
+            };
             $scope.order = $scope.filterSelection.value;
 
         } else if ($scope.filterSelection.value === 'real_name') {
 
-            $scope.filterSelection = {text:' Qualquer Texto', value:'$'};
+            $scope.filterSelection = {
+                text: ' Qualquer Texto',
+                value: '$'
+            };
             $scope.queryFilter = $scope.queryText;
 
         } else {
-        	$scope.filterSelection = {text:'Nome', value:'name'};
+            $scope.filterSelection = {
+                text: 'Nome',
+                value: 'name'
+            };
             $scope.order = $scope.filterSelection.value;
         }
     }
@@ -52,49 +65,56 @@ characterControllers.controller('ListController', ['$scope','ModalService', '$ht
         return false;
     }
 
-    $scope.deleteItem = function(item){
+    $scope.deleteItem = function(item) {
         $scope.showDiv = !$scope.showDiv;
         $scope.characters.splice($scope.characters.indexOf(item), 1);
         $scope.msgDelete = item.name + " Deletado com sucesso";
     }
-       $scope.viewItem = function(item){
+    $scope.viewItem = function(item) {
         console.log('Testing ' + item.name);
     }
 
-$scope.showCustom = function(item) {
+    $scope.showCustom = function(item) {
 
-    ModalService.showModal({
-      templateUrl: "partials/details.html",
-      controller: "CustomController"
-    }).then(function(modal) {
-        console.log(modal);
+        ModalService.showModal({
+            templateUrl: "partials/details.html",
+            controller: "CustomController",
+            inputs: {
+                item: item,
 
-    modal.close.then(function(result) {
-        
-    });
-    
- 
-    });
+            }
+        }).then(function(modal) {
+            console.log(modal);
 
-  };
+            modal.close.then(function(result) {
 
-$scope.confirmation = function(item) {
+            });
 
-    ModalService.showModal({
-      templateUrl: "partials/confirmation.html",
-      controller: "CustomController"
-    }).then(function(modal) {
-console.log('teste' + item.name);
-      modal.element.modal(item);
-      modal.close.then(function(result) {
-       
-      });
-    });
 
-  };
+        });
+
+    };
+
+    $scope.showConfirmation = function(item) {
+
+        ModalService.showModal({
+            templateUrl: "partials/confirmation.html",
+            controller: "CustomController",
+            inputs: {
+                item: item,
+
+            }
+        }).then(function(modal) {
+            console.log('teste' + item.name);
+            modal.element.modal(item);
+            modal.close.then(function(result) {
+
+            });
+        });
+
+    };
 
 
 
 
 }]);
-
